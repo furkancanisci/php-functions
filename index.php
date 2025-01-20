18dad4a012eb75dc63aa9bb10eb0860ebf06a0e3
furkancanisci <99483050+furkancanisci@users.noreply.github.com>
2025-01-20 11:11:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -103,4 +103,69 @@ function array_add($variable, $array)
     return $array;
 }
 
+
+class MyThread extends Thread 
+{
+    private $sleepDuration;
+
+    public function __construct($sleepDuration)
+    {
+        $this->sleepDuration = $sleepDuration;
+    }
+
+    public function run()
+    {
+        echo "Thread " . $this->getThreadId() . " çalışıyor ve " . $this->sleepDuration . " saniye bekliyor." . PHP_EOL;
+        sleep($this->sleepDuration);
+        echo "Thread " . $this->getThreadId() . " işlemi tamamlandı." . PHP_EOL;
+    }
+}
+
+$thread1 = new MyThread(2);
+$thread2 = new MyThread(4);
+$thread3 = new MyThread(1);
+
+$thread1->start();
+$thread2->start();
+$thread3->start();
+
+$thread1->join();
+$thread2->join();
+$thread3->join();
+
+echo "All threads are completed." . PHP_EOL;
+
+
+// Dosyaya yazma işlevi
+function write_to_file($filename, $content)
+{
+    if (file_put_contents($filename, $content) === false) {
+        echo "Dosya yazılamadı: " . $filename . PHP_EOL;
+    } else {
+        echo "Dosyaya yazıldı: " . $filename . PHP_EOL;
+    }
+}
+
+function read_from_file($filename)
+{
+    if (file_exists($filename)) {
+        $content = file_get_contents($filename);
+        if ($content === false) {
+            echo "File cannot read: " . $filename . PHP_EOL;
+        } else {
+            echo "Content from file: " . $content . PHP_EOL;
+            return $content;
+        }
+    } else {
+        echo "File cannot found: " . $filename . PHP_EOL;
+    }
+    return null;
+}
+
+$filename = "example.txt";
+$content = "Hello World!";
+
+write_to_file($filename, $content);
+read_from_file($filename);
+
 ?>
